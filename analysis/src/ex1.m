b78687700314787fc909327d380af0529a1ecb4a
martin jakubik <martin.jakubik@gmail.com>
2017-07-06 23:30:15 +0200

    removed time variables; got a slightly useful graph

--- analysis/src/ex1.m
+++ analysis/src/ex1.m
@@ -1,11 +1,12 @@
 data = csvread('/Users/martin/code/gitwork/headaches/data-preparation/resources/data1-generated-negatives-auto.txt');
 
-X = data(:, 1:9); y = data(:, 10);
+X = data(:, 1:6);
+y = data(:, 7);
 m = length(y); % number of training examples
 
 % Print out some data points
 fprintf('Some 10 examples from the dataset: \n');
-fprintf(' x = [%.0f %.0f %.0f %.0f %.0f %.0f %.0f %.0f %.0f], y = %.0f \n', [X(120:130,:) y(120:130,:)]');
+fprintf(' x = [%.0f %.0f %.0f %.0f %.0f %.0f], y = %.0f \n', [X(120:130,:) y(120:130,:)]');
 
 fprintf('Program paused. Press enter to continue.\n');
 pause;
--- analysis/src/plotData.m
+++ analysis/src/plotData.m
@@ -1,13 +1,18 @@
-function plotData(x, y)
+function plotData(X, y)
 
 % opens a new figure window
-figure;
+figure; hold on;
+
+pos = find(y==1);
+neg = find(y==0);
 
 % plots the data
-plot(x, y, 'rx', 'MarkerSize', 4);
+plot(X(neg, 2), X(neg, 4), 'ko', 'MarkerSize', 2);
+plot(X(pos, 2), X(pos, 4), 'kx', 'MarkerSize', 8, 'MarkerFaceColor', 'r');
+
 
 % sets the y-axis label
-ylabel('Is headache?');
+ylabel('Day of the week');
 
 % sets the x-axis label
 xlabel('Date');
